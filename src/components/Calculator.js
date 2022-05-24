9d33df73b754e63aa6fb70a31c85d212858b82fc
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-05-24 14:28:24 +0100

    Modified the Calculator.js file

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -14,22 +14,22 @@ class Calculator extends Component {
         <button type="button" className="">AC</button>
         <button type="button">+/-</button>
         <button on type="button">%</button>
-        <button type="button" className="br op-col">÷</button>
+        <button type="button" className="br operators">÷</button>
         <button type="button">7</button>
         <button type="button">8</button>
         <button on type="button">9</button>
-        <button type="button" className="op-col">x</button>
+        <button type="button" className="operators">x</button>
         <button type="button">4</button>
         <button type="button">5</button>
         <button on type="button">6</button>
-        <button type="button" className="op-col">-</button>
+        <button type="button" className="operators">-</button>
         <button type="button">1</button>
         <button type="button">2</button>
         <button type="button" className="span-t-b">3</button>
-        <button type="button" className="op-col">+</button>
+        <button type="button" className="operators">+</button>
         <button type="button" className="zero-span2">0</button>
         <button type="button">.</button>
-        <button type="button" className="op-col">=</button>
+        <button type="button" className="operators">=</button>
       </div>
     );
   }
