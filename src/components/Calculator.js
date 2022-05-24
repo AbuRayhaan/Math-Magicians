9e87ad0824dc1ddabdca07a4ec724423935c7b4f
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-05-25 00:59:05 +0100

    Modified the calculator.js to add an event listener

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,36 +1,49 @@
 import { Component } from 'react';
 import './Calculator.css';
-import calculate from './logic/calculate';
+import calculate from '../logic/calculate';
 
 class Calculator extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      operation: null,
+      next: null,
+      result: null,
+    };
+  }
+
+  Calculation(event) {
+    this.calcButtons = event.target.innerText;
+    const calculationButtons = this.calcButtons;
+    const obj = this.state;
+    this.setState(calculate(obj, calculationButtons));
   }
 
   render() {
+    const { operation, next, result } = this.state;
+
     return (
       <div className="calc-container">
-        <div className="output">0</div>
-        <button type="button" className="">AC</button>
-        <button type="button">+/-</button>
-        <button on type="button">%</button>
-        <button type="button" className="br operators">÷</button>
-        <button type="button">7</button>
-        <button type="button">8</button>
-        <button on type="button">9</button>
-        <button type="button" className="operators">x</button>
-        <button type="button">4</button>
-        <button type="button">5</button>
-        <button on type="button">6</button>
-        <button type="button" className="operators">-</button>
-        <button type="button">1</button>
-        <button type="button">2</button>
-        <button type="button" className="span-t-b">3</button>
-        <button type="button" className="operators">+</button>
-        <button type="button" className="zero-span2">0</button>
-        <button type="button">.</button>
-        <button type="button" className="operators">=</button>
+        <div className="output">{ next || operation || result || 0 }</div>
+        <button type="button" onClick={this.Calculation.bind(this)}>AC</button>
+        <button type="button" onClick={this.Calculation.bind(this)}>+/-</button>
+        <button on type="button" onClick={this.Calculation.bind(this)}>%</button>
+        <button type="button" className="br operators" onClick={this.Calculation.bind(this)}>÷</button>
+        <button type="button" onClick={this.Calculation.bind(this)}>7</button>
+        <button type="button" onClick={this.Calculation.bind(this)}>8</button>
+        <button on type="button" onClick={this.Calculation.bind(this)}>9</button>
+        <button type="button" className="operators" onClick={this.Calculation.bind(this)}>x</button>
+        <button type="button" onClick={this.Calculation.bind(this)}>4</button>
+        <button type="button" onClick={this.Calculation.bind(this)}>5</button>
+        <button on type="button" onClick={this.Calculation.bind(this)}>6</button>
+        <button type="button" className="operators" onClick={this.Calculation.bind(this)}>-</button>
+        <button type="button" onClick={this.Calculation.bind(this)}>1</button>
+        <button type="button" onClick={this.Calculation.bind(this)}>2</button>
+        <button type="button" className="span-t-b" onClick={this.Calculation.bind(this)}>3</button>
+        <button type="button" className="operators" onClick={this.Calculation.bind(this)}>+</button>
+        <button type="button" className="zero-span2" onClick={this.Calculation.bind(this)}>0</button>
+        <button type="button" onClick={this.Calculation.bind(this)}>.</button>
+        <button type="button" className="operators" onClick={this.Calculation.bind(this)}>=</button>
       </div>
     );
   }
