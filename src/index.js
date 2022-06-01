246776d88d1ce0069a79e969cb103588ca21fb21
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-06-01 19:50:36 +0100

    Renamed the pages extension to scss

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
-import './pages.css';
+import './pages.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../pages.css';
+import '../pages.scss';
 
 const Home = () => (
   <>
